37e66c393933661d75955d0d5f3b25b66d5b6394
programmercintasunnah <muhammadzakie22g@gmail.com>
2024-10-09 13:17:31 +0700

    bobot_repository.go

---
+++ internal/repository/bobot_repository.go
@@ -0,0 +1,26 @@
+package repository
+
+import (
+	"go-tc-plnsmrng/internal/models"
+
+	"github.com/jmoiron/sqlx"
+)
+
+type BobotRepository struct {
+	db *sqlx.DB
+}
+
+func NewBobotRepository(db *sqlx.DB) *BobotRepository {
+	return &BobotRepository{db: db}
+}
+
+func (r *BobotRepository) CreateBobot(bobot *models.Bobot) error {
+	query := `INSERT INTO bobot (parent_id, nama, nomor) VALUES ($1, $2, $3) RETURNING id`
+	return r.db.QueryRow(query, bobot.ParentID, bobot.Nama, bobot.Nomor).Scan(&bobot.ID)
+}
+
+func (r *BobotRepository) GetAllBobots() ([]models.Bobot, error) {
+	var bobots []models.Bobot
+	err := r.db.Select(&bobots, "SELECT * FROM bobot ORDER BY nomor ASC")
+	return bobots, err
+}
