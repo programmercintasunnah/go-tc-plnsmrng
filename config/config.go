f7789aac6e9da40f6b040b0bcbe10dd5275fed02
programmercintasunnah <muhammadzakie22g@gmail.com>
2024-10-09 13:13:53 +0700

    config.go

--- config/config.go
+++ config/config.go
@@ -1,1 +1,25 @@
 package config
+
+import (
+	"log"
+
+	"github.com/jmoiron/sqlx"
+	_ "github.com/lib/pq"
+)
+
+type Config struct {
+	DB        *sqlx.DB
+	JWTSecret string
+}
+
+func NewConfig() *Config {
+	db, err := sqlx.Connect("postgres", "user=youruser password=yourpassword dbname=yourdb sslmode=disable")
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	return &Config{
+		DB:        db,
+		JWTSecret: "supersecretkey",
+	}
+}
