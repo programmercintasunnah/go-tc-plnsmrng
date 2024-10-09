4067feec7fbe78f276c5f6b589cb6bca4e6da271
programmercintasunnah <muhammadzakie22g@gmail.com>
2024-10-09 13:19:51 +0700

    main.go

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"go-tc-plnsmrng/config"
+	"go-tc-plnsmrng/internal/handlers"
+	"go-tc-plnsmrng/internal/repository"
+	"log"
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+)
 
 func main() {
-	fmt.Println("structur project")
+	cfg := config.NewConfig()
+	repo := repository.NewBobotRepository(cfg.DB)
+	bobotHandler := handlers.NewBobotHandler(repo)
+
+	r := chi.NewRouter()
+
+	r.Post("/bobot", bobotHandler.CreateBobot)
+	r.Get("/bobots", bobotHandler.GetAllBobots)
+
+	log.Println("Starting server on :8080")
+	log.Fatal(http.ListenAndServe(":8080", r))
 }
