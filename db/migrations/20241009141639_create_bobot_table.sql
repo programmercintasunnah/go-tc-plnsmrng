33c42aca7e334b410c84527eeef1a3ad60da5697
programmercintasunnah <muhammadzakie22g@gmail.com>
2024-10-09 21:18:19 +0700

    dbmate db/migrations

---
+++ db/migrations/20241009141639_create_bobot_table.sql
@@ -0,0 +1,10 @@
+-- migrate:up
+CREATE TABLE IF NOT EXISTS bobot (
+    id SERIAL PRIMARY KEY,
+    parent_id INT,
+    name VARCHAR(255) NOT NULL,
+    nomor VARCHAR(50) NOT NULL
+);
+
+-- migrate:down
+DROP TABLE IF EXISTS bobot;
