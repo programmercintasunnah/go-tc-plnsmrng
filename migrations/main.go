90d8c7bcd15df8005e6979f56fe9adee8105701a
programmercintasunnah <muhammadzakie22g@gmail.com>
2024-10-09 07:05:45 +0700

    structur db

--- migrations/main.go
+++ migrations/main.go
@@ -1,1 +1,10 @@
 package migrations
+
+// Struktur Database:
+// CREATE TABLE bobot (
+//     id SERIAL PRIMARY KEY,
+//     parent_id INT REFERENCES bobot(id),
+//     nama VARCHAR(255) NOT NULL,
+//     nomor VARCHAR(255) NOT NULL,
+//     UNIQUE(nomor)
+// );
