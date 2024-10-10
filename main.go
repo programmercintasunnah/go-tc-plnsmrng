95b6c010cd26a5410a77e58625f3b934cd4f14ca
programmercintasunnah <muhammadzakie22g@gmail.com>
2024-10-10 09:00:17 +0700

    deploy ke vercel

--- main.go
+++ main.go
@@ -4,44 +4,34 @@ import (
 	"go-tc-plnsmrng/config"
 	"go-tc-plnsmrng/internal/handlers"
 	"go-tc-plnsmrng/internal/repository"
-	"log"
 	"net/http"
 
 	"github.com/go-chi/chi"
 	"github.com/go-chi/cors"
 )
 
-// Struktur Proyek:
-// /bobot-service
-//   ├── /main.go			   # Entry point
-//   ├── /internal
-//   │     ├── /handlers       # HTTP handlers
-//   │     ├── /models         # Model struct
-//   │     └── /repository     # DB interactions
-//   ├── /config
-//   │     └── /config.go      # Database and JWT config
-//   └── /migrations           # DB migration scripts
-
-func main() {
+func Handler(w http.ResponseWriter, r *http.Request) {
 	cfg := config.NewConfig()
 	repo := repository.NewBobotRepository(cfg.DB)
 	bobotHandler := handlers.NewBobotHandler(repo)
 
-	r := chi.NewRouter()
+	router := chi.NewRouter()
 
 	// Middleware CORS
-	r.Use(cors.Handler(cors.Options{
-		AllowedOrigins:   []string{"*"},                             // Allow all origins (set as needed)
-		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE"},  // HTTP methods that are allowed
-		AllowedHeaders:   []string{"Content-Type", "Authorization"}, // Allowed headers
+	router.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"*"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE"},
+		AllowedHeaders:   []string{"Content-Type", "Authorization"},
 		ExposedHeaders:   []string{"Link"},
 		AllowCredentials: false,
-		MaxAge:           300, // Maximum value for preflight request caching
+		MaxAge:           300,
 	}))
 
-	r.Post("/api/bobot", bobotHandler.CreateBobot)
-	r.Get("/api/bobots", bobotHandler.GetAllBobots)
+	router.Post("/api/bobot", bobotHandler.CreateBobot)
+	router.Get("/api/bobots", bobotHandler.GetAllBobots)
 
 	log.Println("Starting server on :8080")
 	log.Fatal(http.ListenAndServe(":8080", r))
+
+	router.ServeHTTP(w, r) // Menjalankan router sebagai HTTP handler
 }
